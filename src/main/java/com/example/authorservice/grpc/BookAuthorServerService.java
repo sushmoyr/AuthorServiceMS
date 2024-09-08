8b0d61c010f164267136814a0c69cac489ab8ee9
Sushmoy Roy <sushmoy.r@utkorsho.org>
2024-09-08 15:59:41 +0600

    updated codes

--- src/main/java/com/example/authorservice/grpc/BookAuthorServerService.java
+++ src/main/java/com/example/authorservice/grpc/BookAuthorServerService.java
@@ -1,15 +1,18 @@
 package com.example.authorservice.grpc;
 
-import com.example.Author;
-import com.example.BookAuthorServiceGrpc;
+
 import com.example.authorservice.AuthorRepository;
 import io.grpc.stub.StreamObserver;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.server.service.GrpcService;
+import tech.utkorsho.grpc.Author;
+import tech.utkorsho.grpc.BookAuthorServiceGrpc;
 
 import java.util.Optional;
 
+@Slf4j
 @GrpcService
 @RequiredArgsConstructor
 public class BookAuthorServerService extends BookAuthorServiceGrpc.BookAuthorServiceImplBase {
@@ -17,17 +20,19 @@ public class BookAuthorServerService extends BookAuthorServiceGrpc.BookAuthorSer
 
     @Override
     public void getAuthor(Author request, StreamObserver<Author> responseObserver) {
-        Optional<com.example.authorservice.Author> author = repository.findById(request.getId());
-        if (author.isPresent()) {
-            responseObserver.onNext(Author.newBuilder()
-                    .setId(author.get().getId())
-                    .setName(author.get().getName())
-                    .setEmail(author.get().getEmail())
-                    .setBio(author.get().getBio())
-                    .build());
-        } else {
-            responseObserver.onError(new EntityNotFoundException());
+        log.info("Received request to get author with id: {}", request.getId());
+        try {
+            Optional<com.example.authorservice.Author> author = repository.findById(request.getId());
+            if (author.isPresent()) {
+                log.info("Author found");
+                responseObserver.onNext(Author.newBuilder().setId(author.get().getId()).setName(author.get().getName()).setEmail(author.get().getEmail()).setBio(author.get().getBio()).build());
+                responseObserver.onCompleted();
+            } else {
+                log.error("Author not found");
+                responseObserver.onError(new EntityNotFoundException("Author not found"));
+            }
+        } catch (Exception e) {
+            responseObserver.onError(e);
         }
-        responseObserver.onCompleted();
     }
 }
